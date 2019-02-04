5e9beaf5985a5f6b72cc26d2b2a4d86fb4bdd7ff
Alex Vakhov <alex.vakhov@gmail.com>
2019-02-04 15:14:28 +0500

    ФТ теперь проверяет возможность ввести элемент списка

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -22,15 +24,30 @@ class NewVisitorTest(unittest.TestCase):
         # Она видит, что заголовок и шапка говорят о списках
         # неотложных дел
         self.assertIn('To-Do', self.browser.title)
-        # self.fail('Закончить тест')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # Ей сразу же предлагается ввести элемент списка
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            input_box.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # Она набирает в текстовом поле "Купить павлиньи перья" (её хобби -
         # вязание рыболовных мушек)
+        input_box.send_keys('Купить павлиньи перья')
 
         # Когда она нажимает enter, страница обновляется, и теперь страница
         # содержит "1: Купить павлиньи перья" в качестве элемента списка
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.test == '1: Купить павлиньи перья' for row in rows)
+        )
 
         # Текстовое поле порежнему приглашает её добавить ещё один элемент.
         # Она вводит "Сделать мушку из павлиньих перьев"
