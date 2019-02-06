d54f564d93b6ab3085d7d5f08dd664b3bdbef97c
Alex Vakhov <alex.vakhov@gmail.com>
2019-02-06 13:55:45 +0500

    Настроен исполнитель ФТ для тестирования промежуточного сервера

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
+import os
 
 MAX_WHITE = 10
 
@@ -14,6 +15,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         """установка"""
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'https://' + staging_server
 
     def tearDown(self):
         """демонтаж"""
